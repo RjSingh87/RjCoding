8a0d8e4583fcbb2ab6984dc47b641c566795b6ff
RjSingh87 <140267956+RjSingh87@users.noreply.github.com>
2023-07-22 23:38:15 +0530

    Create Screen.js

---
+++ ReactNativeProject/Screen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Screen = () => {
+  return (
+    <View>
+      <Text>Screen</Text>
+      <Text>About</Text>
+      <Text>Setting</Text>
+    </View>
+  )
+}
+
+export default Screen
+
+const styles = StyleSheet.create({})
